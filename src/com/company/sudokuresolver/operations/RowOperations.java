e957a0324fa3f8bee13b302b31d0b96df0ccd775
McAlister <johmcali@amazon.com>
2017-06-10 13:29:19 +0100

    Number can go in only one position in Row

--- src/com/company/sudokuresolver/operations/RowOperations.java
+++ src/com/company/sudokuresolver/operations/RowOperations.java
@@ -2,6 +2,10 @@ package com.company.sudokuresolver.operations;
 
 import com.company.sudokuresolver.IndividualEntry;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 import static com.company.sudokuresolver.Utility.DIMENSION;
 
 public class RowOperations {
@@ -13,5 +17,41 @@ public class RowOperations {
                 grid[rowIndex][i].eliminatePossibility(number);
             }
         }
+    }
+
+    public static void populateIfNumberCanOnlyGoInOnePosition(final IndividualEntry[][] grid) {
+        //for a given row
+        //Get possible numbers
+        //When going through iteration eliminate 'possible' number on second appearance
+        //Loop again and see if remaining possible number can slot into each available slot.
+        ArrayList<Integer> possibleNumbers = new ArrayList<Integer>();
+
+        for (int i = 0; i < DIMENSION; i++) {
+            for (int j = 0; j < DIMENSION; j++) {
+                if (!grid[i][j].isFilled()) {
+                    for (int number = 0; number < DIMENSION; number++) {
+                        if (grid[i][j].getPossibleNumbers()[number]) {
+                            if (possibleNumbers.contains(number)) {
+                                possibleNumbers.remove(Integer.valueOf(number));
+                            } else {
+                                possibleNumbers.add(number);
+                            }
+                        }
+                    }
+                }
+            }
+            int onlyPossibleNumber;
+            if (possibleNumbers.size() == 1) {
+                onlyPossibleNumber = possibleNumbers.get(0);
+                for (int j = 0; j < DIMENSION; j++) {
+                    if(grid[i][j].getPossibleNumbers()[onlyPossibleNumber]){
+                        grid[i][j].setNumber(onlyPossibleNumber);
+                    }
+                }
+
+            }
+        }
+
+
     }
 }
---
+++ tst/com/company/sudokuresolver/operations/RowOperationsTest.java
@@ -0,0 +1,47 @@
+package com.company.sudokuresolver.operations;
+
+import com.company.sudokuresolver.SudokuGrid;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RowOperationsTest {
+
+    @Test
+    public void testPopulateGivenTwoCanOnlyGoInOnePlace() {
+        SudokuGrid sudokuGrid = new SudokuGrid();
+
+        sudokuGrid.putNumber(0, 0, 1);
+        sudokuGrid.putNumber(0, 2, 3);
+        sudokuGrid.putNumber(0, 3, 4);
+        sudokuGrid.putNumber(0, 5, 6);
+        sudokuGrid.putNumber(0, 6, 7);
+
+        sudokuGrid.putNumber(1, 1, 2);
+        sudokuGrid.putNumber(4, 4, 2);
+        sudokuGrid.putNumber(8, 7, 2);
+
+        RowOperations.populateIfNumberCanOnlyGoInOnePosition(sudokuGrid.getGrid());
+
+        assertEquals(Integer.valueOf(2), sudokuGrid.getGrid()[0][8].getNumber());
+    }
+
+    @Test
+    public void testPopulateGivenSixCanOnlyGoInOnePlace() {
+        SudokuGrid sudokuGrid = new SudokuGrid();
+
+        sudokuGrid.putNumber(0, 0, 1);
+        sudokuGrid.putNumber(0, 2, 3);
+        sudokuGrid.putNumber(0, 3, 4);
+
+        sudokuGrid.putNumber(1, 1, 6);
+        sudokuGrid.putNumber(2, 4, 6);
+        sudokuGrid.putNumber(3, 5, 6);
+        sudokuGrid.putNumber(4, 6, 6);
+        sudokuGrid.putNumber(8, 7, 6);
+
+        RowOperations.populateIfNumberCanOnlyGoInOnePosition(sudokuGrid.getGrid());
+
+        assertEquals(Integer.valueOf(6), sudokuGrid.getGrid()[0][8].getNumber());
+    }
+}
